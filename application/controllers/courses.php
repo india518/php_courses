f803099399858dd99ddb1b0d67ca5407666b9a6c
India Meisner <india.rae@gmail.com>
2013-09-30 20:18:46 -0700

    Create course database, course model (datamapper model) and courses controller. Albe to use ORM to get all courses from the database and display in controller index

---
+++ application/controllers/courses.php
@@ -0,0 +1,30 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Courses extends CI_Controller {
+
+	function __construct()
+    {
+        parent::__construct();
+		//This cannot be used with Ajax, but if we disable Ajax it will
+		// give us information about speeding up our queries:
+		$this->output->enable_profiler(TRUE);
+    }
+
+    function index()
+    {
+    	//$this->load->model('course');
+    	$courses = new Course();
+    	$data['courses'] = $courses->get();
+
+    	echo "<h2>index view test</h2>";
+    	foreach($data['courses'] as $course)
+    	{
+    		echo "<p>{$course->title}: {$course->course_description}</p>";
+    	}
+
+    	//$this->load->view('courses_index', $data);
+    }
+
+}
+
+//end of file
