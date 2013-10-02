f4182795962cf1771f785baaae2c6447eab17bde
India Meisner <india.rae@gmail.com>
2013-10-01 21:48:19 -0700

    Fix minor issue with 'Cancel' button on Add/Edit course form, it now always reloads the base_url(). Edit function now works, although I'm not sure I'm happy with it. Still need AJAX; but can we improve anything before bringing in AJAX?

--- application/controllers/courses.php
+++ application/controllers/courses.php
@@ -7,17 +7,18 @@ class Courses extends CI_Controller {
         parent::__construct();
 		//This cannot be used with Ajax, but if we disable Ajax it will
 		// give us information about speeding up our queries:
-		//$this->output->enable_profiler(TRUE);
+		$this->output->enable_profiler(TRUE);
     }
 
     function index()
     {
     	$courses = new Course();
     	$data['courses'] = $courses->get(); //seems to be ordered by id, by default
+        $data['course_edit'] = FALSE; //we are not editing; see edit_course_form()
     	$this->load->view('courses_index', $data);
     }
 
-    function process_course_form()
+    function new_course_form()
     {
     	//now that we're using Datamapper, we use the validations in the model
     	//http://datamapper.wanwizard.eu/pages/validation.html#
@@ -60,9 +61,42 @@ _HTML
 
     function edit_course()
     {
-        $messages = ['Edit form submitted.', 'Edit functionality in progress.'];
-        $this->session->set_flashdata('error_messages', $messages);
-        redirect(base_url());
+        //get the object from the database
+        $course = new Course();
+        $course->where('id', $this->input->post('course_id'))->get();
+
+        //load page with course info for form
+        //Need to DRY some of this up... how?
+        $courses = new Course();
+        $data['courses'] = $courses->get(); //seems to be ordered by id, by default
+        // here is our info for the form, since we are editting a course
+        $data['course_edit'] = $course;
+        $this->load->view('courses_index', $data);
+    }
+
+    function update_course_form()
+    {
+        //while getting this to work, lets create some helpful variables we can delete later:
+        $id = $this->input->post('course_id');
+        $title = $this->input->post('title');
+        $description = $this->input->post('course_description');
+
+        //get the object from the database
+        $course = new Course();
+        $course->where('id', $id)->get();
+        $course->title = $title;
+        $course->course_description = $description;
+        if ($course->save()) //hmm, note that this is like rails!
+        {
+            redirect(base_url());
+        }
+        else
+        {
+            var_dump($course->error->all);
+            die();
+            $this->session->set_flashdata('error_messages', $course->error->all);
+            redirect(base_url());
+        }
     }
 
     function delete_course()
@@ -73,7 +107,7 @@ _HTML
         
         //delete object
         $course->delete();
-        redirect(base_url());
+        redirect(base_url());   //will change this for AJAX
     }
 }
 
--- application/views/courses_index.php
+++ application/views/courses_index.php
@@ -3,10 +3,10 @@
 <head>
 	<meta charset="UTF-8">
 	<title>Course List</title>
-	<script type="text/javascript" src="assets/js/jquery.1_10_2.js"></script>
-	<script type="text/javascript" src="assets/js/courses.js"></script>
-	<link href="assets/css/bootstrap.min.css" rel="stylesheet" type="text/css">
-	<link href="assets/css/mycss.css" rel="stylesheet" type="text/css">
+	<script type="text/javascript" src="<?= base_url() ?>/assets/js/jquery.1_10_2.js"></script>
+	<script type="text/javascript" src="<?= base_url() ?>/assets/js/courses.js"></script>
+	<link href="<?= base_url() ?>/assets/css/bootstrap.min.css" rel="stylesheet" type="text/css">
+	<link href="<?= base_url() ?>/assets/css/mycss.css" rel="stylesheet" type="text/css">
 </head>
 <body>
 	<div id="wrapper" class="container">
@@ -19,28 +19,75 @@
 <?php		}	?>
 		</div><!-- closes the alert block -->
 <?php	}	?>
-		<form id="add_course" class="form-horizontal span5" action="courses/process_course_form" method="post">
+		<form id="add_or_edit_course" class="form-horizontal span5" 
+<?php 		if ($course_edit)
+			{	?>
+				action="update_course_form" 
+<?php		}
+			else
+			{	?>
+				action="courses/new_course_form" 
+<?php		}	?>
+				method="post">
 			<div class="control-group">
 				<div class="controls">
+<?php 			if ($course_edit)
+				{	?>
+					<legend class="span3">Edit Course</legend>
+<?php 			}
+				else
+				{	?>
 					<legend class="span3">Add Course</legend>
+<?php			}	?>
 				</div>
 			</div>
 			<div class="control-group">
 				<label class="control-label" for="title">Title:</label>
 				<div class="controls">
-					<input class="span3" type="text" name="title" id="title" placeholder="Course Title" />
+					<input class="span3" type="text" name="title" id="title" 
+<?php 				if ($course_edit)
+					{	?>
+						value="<?= $course_edit->title ?>"
+<?php				}
+					else
+					{	?>
+						placeholder="Course Title"
+<?php				}	?>
+						/> <!-- closing the input tag -->
 				</div>
 			</div>
 			<div class="control-group">
 				<label class="control-label" for="course_description">Description:</label>
 				<div class="controls">
-					<textarea class="span3" rows="5" name="course_description" id="course_description" placeholder="Course Description"></textarea>
+					<textarea class="span3" rows="5" name="course_description" id="course_description" 
+<?php 				if ($course_edit)
+					{	?>
+						><?= $course_edit->course_description ?></textarea>
+<?php				}
+					else
+					{	?>
+						placeholder="Course Description"></textarea>
+<?php				}	?>
 				</div>
 			</div>
 			<div class="control-group">
 				<div class="controls">
-					<a id="cancel_form" href="#" class="btn btn-danger">Cancel</a>
-					<button type="submit" class="btn btn-primary pull-right">Add Course</button>
+					<a id="cancel_form" href="<?= base_url() ?>" class="btn btn-danger">Cancel</a>
+					<!-- include a hidden field for id, if we are editing: -->
+<?php 				if ($course_edit)
+					{	?>
+						<input type="hidden" name="course_id" value="<?= $course_edit->id ?>" />
+<?php				}	?>
+					<button type="submit" class="btn btn-primary pull-right">
+<?php 				if ($course_edit)
+					{	?>
+						Update Course
+<?php				}
+					else
+					{	?>
+						Add Course
+<?php				}	?>
+					</button>
 				</div>
 			</div>
 		</form> <!-- End of new course form -->
